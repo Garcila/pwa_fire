5f739e321f77ee3d470778c7126f3958295f43a4
German <germanarcila@gmail.com>
2022-04-23 10:57:04 -0400

    add nasa site info and change service worker

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,82 @@
-const staticUnicorn = "uni-site";
-const assets = [
+const CACHE_NAME = "nasa_past";
+const DATA_CACHE_NAME = "data-cache";
+const FILES_TO_CACHE = [
 	"/",
 	"/index.html",
+	"/favicon.ico",
 	"/icon-192x192.png",
 	"/icon-256x256.png",
 	"/icon-384x384.png",
 	"/icon-512x512.png",
 ];
 
-self.addEventListener("install", installEvent => {
-	installEvent.waitUntil(
-		caches.open(staticUnicorn).then(cache => {
-			cache.addAll(assets);
+// Install the service worker
+self.addEventListener("install", function (evt) {
+	evt.waitUntil(
+		caches.open(CACHE_NAME).then(cache => {
+			console.log("Your files were pre-cached successfully!");
+			return cache.addAll(FILES_TO_CACHE);
 		})
 	);
+
+	self.skipWaiting();
 });
 
-self.addEventListener("fetch", fetchEvent => {
-	fetchEvent.respondWith(
-		caches.match(fetchEvent.request).then(res => {
-			return res || fetch(fetchEvent.request);
+// Activate the service worker and remove old data from the cache
+self.addEventListener("activate", function (evt) {
+	evt.waitUntil(
+		caches.keys().then(keyList => {
+			return Promise.all(
+				keyList.map(key => {
+					if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+						console.log("Removing old cache data", key);
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
+
+	self.clients.claim();
+});
+
+// Intercept fetch requests
+self.addEventListener("fetch", function (evt) {
+	if (evt.request.url.includes("/api/")) {
+		evt.respondWith(
+			caches
+				.open(DATA_CACHE_NAME)
+				.then(cache => {
+					return fetch(evt.request)
+						.then(response => {
+							// If the response was good, clone it and store it in the cache.
+							if (response.status === 200) {
+								cache.put(evt.request.url, response.clone());
+							}
+
+							return response;
+						})
+						.catch(err => {
+							// Network request failed, try to get it from the cache.
+							return cache.match(evt.request);
+						});
+				})
+				.catch(err => console.log(err))
+		);
+
+		return;
+	}
+
+	evt.respondWith(
+		fetch(evt.request).catch(function () {
+			return caches.match(evt.request).then(function (response) {
+				if (response) {
+					return response;
+				} else if (evt.request.headers.get("accept").includes("text/html")) {
+					// return the cached home page for all requests for html pages
+					return caches.match("/");
+				}
+			});
 		})
 	);
 });
